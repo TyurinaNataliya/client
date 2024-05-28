29f80c63db3db559fe0684d049e15c4bd9bff500
DESKTOP-7M7TP19\Nataly <nata-tyurina>
2024-05-28 15:11:14 +0300

    Коррекция

--- routes/diagrammRouter.js
+++ routes/diagrammRouter.js
@@ -1,10 +1,10 @@
-const Router = require('express');
-const DiagrammController = require('../controllers/diagrammController');
+const Router = require("express");
+const DiagrammController = require("../controllers/diagrammController");
 
 const router = new Router();
 
-router.post('/', DiagrammController.postDiagramm )
-router.get('/', DiagrammController.getAll)
-router.get('/:id', DiagrammController.getDiagramm)
-router.delete('/',DiagrammController.deleteDiagramm)
-module.exports = router
+router.post("/", DiagrammController.postDiagramm);
+router.get("/", DiagrammController.getAll);
+router.get("/:id", DiagrammController.getDiagramm);
+router.delete("/:id", DiagrammController.deleteDiagramm);
+module.exports = router;
